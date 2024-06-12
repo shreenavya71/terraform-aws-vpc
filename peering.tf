6044fef4a108318d6c08cff8d62e6e1d4f47d099
shreenavya71 <85473764+shreenavya71@users.noreply.github.com>
2024-06-12 13:18:19 +0100

    new commit

--- peering.tf
+++ peering.tf
@@ -1,7 +1,7 @@
 resource "aws_vpc_peering_connection" "peering" {
     count = var.is_peering_required ? 1 : 0
     vpc_id        = aws_vpc.main.id   # requestor vpc
-    peer_vpc_id   = var.acceptor_vpc_id == "" ? "data.aws_vpc.default.id" : var.acceptor_vpc_id
+    peer_vpc_id   = var.acceptor_vpc_id == "" ? data.aws_vpc.default.id : var.acceptor_vpc_id
     auto_accept = var.acceptor_vpc_id == "" ? true : false
     tags = merge(
         var.common_tags,
